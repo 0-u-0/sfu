6fe058d4059258ba5c58e7b7f129410a6134f3dd
cc <craterone11@gmail.com>
2022-06-18 13:05:11 +0800

    rm log4cxx

--- deps/logging-log4cxx/src/cmake/projectVersionDetails.cmake
+++
@@ -1,5 +0,0 @@
-# This file should contain nothing but the following line
-# setting the project version. The variable name must not
-# clash with the log4cxx_VERSION* variables automatically
-# defined by the project() command.
-set(log4cxx_VER 0.12.0.0)
--- deps/logging-log4cxx/src/cmake/pthread/log4cxx-pthread.cmake
+++
@@ -1,6 +0,0 @@
-include(FindThreads)
-
-try_compile(PTHREAD_SETNAME_NP_FOUND "${CMAKE_BINARY_DIR}/pthread-compile-tests"
-    "${CMAKE_CURRENT_LIST_DIR}/test-pthread.cpp"
-    LINK_LIBRARIES Threads::Threads )
-
